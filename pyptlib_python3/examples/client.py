4b3e5ef2495f9743112fbc2745dfc59c6c7a5cc1
Alexander Mages <magesalexander123@gmail.com>
2021-07-29 10:11:28 -0500

    pyptlib library import fixes

---
+++ pyptlib_python3/examples/client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""This is a client-side example of the pyptlib API."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+
+from pyptlib.client import ClientTransportPlugin
+from pyptlib.config import EnvError
+
+if __name__ == '__main__':
+    client = ClientTransportPlugin()
+    try:
+        client.init(["blackfish", "bluefish"])
+    except EnvError as err:
+        print("pyptlib could not bootstrap ('%s')." % str(err))
+        sys.exit(1)
+
+    for transport in client.getTransports():
+        # Spawn all the transports in the list, and for each spawned
+        # transport report back the port where it is listening, and
+        # the SOCKS version it supports.
+
+        try:
+            socks_version, bind_addrport = your_function_that_launches_transports(transport)
+        except YourFailException as err:
+            reportFailure(transport, "Failed to launch ('%s')." % str(err))
+            continue
+
+        client.reportMethodSuccess(transport, socks_version, bind_addrport, None, None)
+
+    # After spawning our transports, report that we are done.
+    client.reportMethodsEnd()
